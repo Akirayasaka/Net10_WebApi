bbe4b4b57a1ef0e834e7551aa9869f8d9dcb369a
Akira <kuramal12@msn.com>
2026-02-05 15:57:49 +0800

    加入專案檔案。

---
+++ Net10_WebApi/Controllers/VillaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Net10_WebApi.Controllers
+{
+    [Route("api/villa")]
+    [ApiController]
+    public class VillaController: ControllerBase
+    {
+        [HttpGet]
+        public string GetVillas()
+        {
+            return "This is the GetVillas method of VillaController.";
+        }
+
+        [HttpGet("{id:int}")]
+        public string GetVillaById(int id)
+        {
+            return $"Get VillaID: {id}";
+        }
+    }
+}
---
+++ Net10_WebApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Net10_WebApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
