2c79d271a42408e142a745e2fcb7618f8427690c
Akira <kuramal12@msn.com>
2026-02-12 10:39:16 +0800

    Get Data by ID

--- Net10_WebApi/Controllers/VillaController.cs
+++ Net10_WebApi/Controllers/VillaController.cs
@@ -7,7 +7,7 @@ namespace Net10_WebApi.Controllers
 {
     [Route("api/villa")]
     [ApiController]
-    public class VillaController: ControllerBase
+    public class VillaController : ControllerBase
     {
         private readonly ApplicationDbContext _db;
 
@@ -16,17 +16,45 @@ namespace Net10_WebApi.Controllers
             _db = db;
         }
 
-
+        /// <summary>
+        /// Get all Villas from the database. No parameters required.
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Villa>>> GetVillas()
         {
             return Ok(await _db.Villa.ToListAsync());
         }
 
+        /// <summary>
+        /// Retrieves the details of a villa by its unique identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier of the villa to retrieve. Must be a positive integer.</param>
+        /// <returns></returns>
         [HttpGet("{id:int}")]
-        public string GetVillaById(int id)
+        public async Task<ActionResult<Villa>> GetVillaById(int id)
         {
-            return $"Get VillaID: {id}";
+            try
+            {
+                if (id <= 0)
+                {
+                    return BadRequest("Invalid villa ID. ID must be a positive integer.");
+                }
+
+                var villa = await _db.Villa.FirstOrDefaultAsync(x => x.Id == id);
+
+                if (villa == null)
+                {
+                    return NotFound($"Villa with ID {id} not found.");
+                }
+
+                return Ok(villa);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, $"An error occurred while retrieving villa with ID {id}: {ex.Message}");
+            }
         }
     }
 }
